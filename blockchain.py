baf78e272640b5940cc4653276a819d4709dff32
yooodleee <accia25@naver.com>
2025-07-08 17:09:10 +0900

    [SBC-2] Block 클래스 구현

---
+++ blockchain.py
@@ -0,0 +1,16 @@
+import hashlib
+import time
+
+
+class Block:
+    def __init__(self, index, transactions, previous_hash, nonce=0):
+        self.index = index
+        self.timestamp = time.time()
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+        self.hash = self.calculate_hash()
+    
+    def calculate_hash(self):
+        block_data = f"{self.index}{self.timestamp}{self.transactions}{self.previous_hash}{self.nonce}"
+        return hashlib.sha256(block_data.encode()).hexdigest()
